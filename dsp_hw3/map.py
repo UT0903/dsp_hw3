6c1e7e592ba9ad9d330e1595c57c985261589274
UT0903 <b07902048@ntu.edu.tw>
2020-12-01 12:03:57 +0800

    new

---
+++ dsp_hw3/map.py
@@ -0,0 +1,21 @@
+import sys
+
+dic = {}
+with open('Big5-ZhuYin.map','r', encoding = 'big5-hkscs') as fp:
+	all_lines = fp.readlines()
+	for line in all_lines:
+		first, secs = line.split()
+		secs = secs.split('/')
+		if first not in dic:
+			dic[first] = [first]
+		for sec in secs:
+			if sec[0] not in dic:
+				dic[sec[0]] = [first]
+			else:
+				dic[sec[0]].append(first)
+
+
+with open('ZhuYin-Big5.map','w', encoding = 'big5-hkscs') as fp:
+	for key, value in dic.items():
+		output = key + ' ' + ' '.join(value) + '\n'
+		fp.write(output)
